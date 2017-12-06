5ae8898a9ff6b068c8e2d72e46c62315b69ac0ef
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2017-12-06 22:36:38 +0900

    Add asset of color class

---
+++ Assets/Colors.cs
@@ -0,0 +1,28 @@
+using System;
+using Discord;
+
+namespace AcidChicken.Samurai.Assets
+{
+    public static class Colors
+    {
+        public static Color Blue { get; } = new Color(0x0366d6);
+
+        public static Color GrayLight { get; } = new Color(0x6a737d);
+
+        public static Color Gray { get; } = new Color(0x586069);
+
+        public static Color GrayDark { get; } = new Color(0x24292e);
+
+        public static Color Green { get; } = new Color(0x28a745);
+
+        public static Color Orange { get; } = new Color(0xa04100);
+
+        public static Color OrangeLight { get; } = new Color(0xe36209);
+
+        public static Color Purple { get; } = new Color(0x6f42c1);
+
+        public static Color Red { get; } = new Color(0xcb2431);
+
+        public static Color White { get; } = new Color(0xffffff);
+    }
+}
