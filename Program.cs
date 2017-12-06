a85644a178c0a675a6f13957423fddbeffe948b4
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2017-12-06 21:14:34 +0900

    Add project file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcidChicken.Samurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
