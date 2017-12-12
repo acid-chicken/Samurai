43175e3a718c646088eefcc1f45ce049ac5f7f12
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2017-12-12 23:35:02 +0900

    Fix conversion module mistakes

--- Samurai/Modules/ConversionModule.cs
+++ Samurai/Modules/ConversionModule.cs
@@ -25,6 +25,7 @@ namespace AcidChicken.Samurai.Modules
             var beforeL = before.ToLower();
             var afterU = after.ToUpper();
             var beforeU = before.ToUpper();
+            var isAfterBTC = afterL == "bitcoin" || afterU == "BTC";
             var isAfterUSD = afterU == UnitedStatesDollar;
             var isAfterC = Convertable.Contains(afterU);
             var isBeforeUSD = beforeU == UnitedStatesDollar;
@@ -90,12 +91,20 @@ namespace AcidChicken.Samurai.Modules
                 }
                 else
                 {
-                    var afterDs = await ApiManager.GetTickersAsDictionaryAsync().ConfigureAwait(false);
                     var beforeDs = await ApiManager.GetTickersAsDictionaryAsync().ConfigureAwait(false);
-                    var afterBD = afterDs.First(x => x["id"] == beforeL || x["name"] == before || x["symbol"] == beforeU);
                     var beforeBD = beforeDs.First(x => x["id"] == afterL || x["name"] == after || x["symbol"] == afterU);
-                    result *= decimal.Parse(afterBD["price_usd"]) / decimal.Parse(beforeBD["price_usd"]);
-                    route = $"{beforeU} > BTC > {afterU}";
+                    if (isAfterBTC)
+                    {
+                        result *= decimal.Parse(beforeBD["price_btc"]);
+                        route = $"{beforeU} > BTC";
+                    }
+                    else
+                    {
+                        var afterDs = await ApiManager.GetTickersAsDictionaryAsync().ConfigureAwait(false);
+                        var afterBD = afterDs.First(x => x["id"] == beforeL || x["name"] == before || x["symbol"] == beforeU);
+                        result *= decimal.Parse(afterBD["price_usd"]) / decimal.Parse(beforeBD["price_usd"]);
+                        route = $"{beforeU} > BTC > {afterU}";
+                    }
                 }
             }
             await ReplyAsync
