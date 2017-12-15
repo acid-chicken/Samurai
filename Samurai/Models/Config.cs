3e450babdf50560bdc6a82bc38faed8c74b5ae68
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2017-12-16 03:57:05 +0900

    Add tipping system

--- Samurai/Models/Config.cs
+++ Samurai/Models/Config.cs
@@ -14,11 +14,23 @@ namespace AcidChicken.Samurai.Models
         [JsonProperty("notification_channels")]
         public HashSet<ulong> NotificationChannels { get; set; } = new HashSet<ulong>();
 
+        [JsonProperty("queue")]
+        public List<TipQueue> Queue { get; set; } = new List<TipQueue>();
+
         // [JsonProperty("rollbar_environment")]
         // public string RollbarEnvironment { get; set; } = "production";
 
         // [JsonProperty("rollbar_token")]
         // public string RollbarConfig { get; set; } = "";
+
+        [JsonProperty("rpc_password")]
+        public string RpcPassword { get; set; } = "";
+
+        [JsonProperty("rpc_server")]
+        public string RpcServer { get; set; } = "http://127.0.0.1:9305";
+
+        [JsonProperty("rpc_user")]
+        public string RpcUser { get; set; } = "";
 
         [JsonProperty("targets")]
         public Dictionary<string, Monitor> Monitors { get; set; } = new Dictionary<string, Monitor>();
@@ -38,5 +50,29 @@ namespace AcidChicken.Samurai.Models
 
         [JsonProperty("last_status")]
         public IPStatus LastStatus { get; set; } = IPStatus.Unknown;
+    }
+
+    [JsonObject]
+    public class TipQueue
+    {
+        public TipQueue(ulong from, ulong to, DateTimeOffset limit, decimal amount)
+        {
+            From = from;
+            To = to;
+            Limit = limit;
+            Amount = amount;
+        }
+
+        [JsonProperty("from")]
+        public ulong From { get; set; }
+
+        [JsonProperty("to")]
+        public ulong To { get; set; }
+
+        [JsonProperty("limit")]
+        public DateTimeOffset Limit { get; set; }
+
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
     }
 }
